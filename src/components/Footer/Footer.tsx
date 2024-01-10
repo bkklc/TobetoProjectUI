3f386a2e5c38dea8061a3a8a6b0c03007f8f0df7
Hoixi <furkan.kayali.1905@gmail.com>
2024-01-10 19:38:40 +0300

    UI

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <footer className='bg-secondary py-5'>
+        <Container>
+          <div className="row justify-content-between align-items-center">
+            <div className="col-6 col-lg-4 mb-lg-0 my-4"><a className="d-block mobile-text-center navbar-brand" href="#">
+              <span>
+                <span >
+                  <img alt="" aria-hidden="true"
+                    width={100}
+                    height={21}
+                    src="https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FTebeto-logo-yatay-beyaz.8c2d6927.png&w=128&q=75" />
+                </span>
+              </span></a></div>
+            <div className="col-6 col-lg-8 my-4">
+              <ul className="list-unstyled mb-0 d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
+                <li className=" mb-lg-0"><a className="btn btn-light-light" href="#">Bize Ulaşın</a></li>
+              </ul>
+            </div>
+            <div className="col-12">
+              <p className="d-lg-none small text-white text-center">© 2022 Tobeto </p>
+            </div>
+          </div>
+        </Container>
+      </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/NavbarComponent.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown, Dropdown } from 'react-bootstrap';
+
+function NavbarComponent() {
+  return (
+    <Navbar expand="sm" className='position-relative navbar navbar-expand-xxl py-5 bg-white'>
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftobeto-logo.409772fc.png&w=384&q=75"
+              width="170"
+              height="35"
+              className="d-inline-block align-top mr-2"
+            />{' '}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              className="mx-auto my-lg-0 d-none d-xxl-flex navbar-nav"
+              style={{ maxHeight: "100px" }}
+              navbarScroll>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#profile">Profile</Nav.Link>
+              <Nav.Link href="#applications">Applications</Nav.Link>
+              <Nav.Link href="#evaluations">Evaluations</Nav.Link>
+              <Nav.Link href="#calendar">Calendar</Nav.Link>
+              <Nav.Link href="#calendar">Calendar</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Nav>
+            <NavDropdown
+              title={
+                <div className="btn-group header-avatar">
+                  <img className="thumbnail-image"
+                    src='https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimages.19a45d39.png&w=48&q=75'
+                    width={32}
+                    height={32}
+                  />
+                  Furkan Kayali
+                </div>
+              }
+              id="basic-nav-dropdown">
+              <Dropdown.Item eventKey={1.1} href="/profile">Profile</Dropdown.Item>
+              <Dropdown.Item eventKey={1.3}>
+                <i className="fa fa-sign-out"></i> Logout
+              </Dropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+  )
+}
+
+export default NavbarComponent
