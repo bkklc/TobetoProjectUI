ca69fcb20dfdae74495501b1b3340c2405a0df10
Hoixi <furkan.kayali.1905@gmail.com>
2024-01-15 17:46:27 +0300

    Login&Register

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+
+export default function Login() {
+    return (
+
+        <>
+        
+        </>
+
+    )
+}
+
---
+++ src/components/Login/Register.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
+
+export default function Register() {
+  return (
+    <>
+    
+    </>
+  )
+}
