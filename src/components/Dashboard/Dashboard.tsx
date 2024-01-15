8263e88eafaf3ed25c03628b7915f6e414fd5425
bkklc <abkklc@gmail.com>
2024-01-15 19:05:32 +0300

    ProfileInfo Pages

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "../../pages/Homepage/Homepage";
 import Degerlendirmeler from "../../pages/Degerlendirmeler/Degerlendirmeler";
 import MyProfile from "../../pages/MyProfile/MyProfile";
-import ProfileInfo from "../ProfileInfo/ProfileInfo";
+import ProfileInfo from "../../pages/ProfileInfo/ProfileInfo";
 
 
 
@@ -12,7 +12,7 @@ export default function Dashboard() {
         <Route path='/' Component={Homepage}></Route>
         <Route path="/degerlendirmeler" Component={Degerlendirmeler}></Route>
         <Route path="/profilim" Component={MyProfile}></Route>
-        <Route path = "/profilBilgileri/" Component={ProfileInfo}></Route>
+        <Route path ="/profilBilgileri/" Component={ProfileInfo}></Route>
      </Routes>
   )
 }
