fd7483da85286aa2ab452647c506e8e1c71ecf55
Beyza Altıntoprak <cicurit@gmail.com>
2024-01-16 23:04:35 +0300

    Experiences submenu is added

---
+++ src/components/ProfileInfo/Experiences.tsx
@@ -0,0 +1,59 @@
+import { Col, Row, Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+
+const Experiences = () => {
+  return (
+    <Col xs={12} lg={9} style={{ minHeight: '90vh' }}>
+      <Form action="#" data-hs-cf-bound="true">
+        <Row className="mb-2">
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">Kurum Adı*</Form.Label>
+            <FormControl name="corporationName" className="form-control tobeto-input" type="text" placeholder="Kampüs 365" />
+          </Col>
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">Pozisyon*</Form.Label>
+            <FormControl name="position" className="form-control tobeto-input" type="text" placeholder="Front-End Developer" />
+          </Col>
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">Sektör*</Form.Label>
+            <FormControl name="sector" className="form-control tobeto-input" type="text" placeholder="Yazılım" />
+          </Col>
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">Şehir Seçiniz*</Form.Label>
+            <Form.Select name="country" className="form-select tobeto-input" aria-label="">
+              <option value="">İl Seçiniz</option>
+            </Form.Select>
+          </Col>
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">İş Başlangıcı*</Form.Label>
+            <InputGroup>
+              <FormControl as="input" type="text" placeholder="gg.aa.yyyy" className="form-control tobeto-input" value="" />
+            </InputGroup>
+          </Col>
+          <Col xs={12} md={6} className="mb-6">
+            <Form.Label className="input-label-text">İş Bitiş*</Form.Label>
+            <InputGroup>
+              <FormControl as="input" type="text" placeholder="gg.aa.yyyy" disabled className="form-control tobeto-input" value="" />
+              <InputGroup.Text>
+                <Form.Check
+                  name="checkbox"
+                  className="form-check-input me-4"
+                  type="checkbox"
+                  label={<small className="text-muted">Çalışmaya Devam Ediyorum</small>}
+                />
+              </InputGroup.Text>
+            </InputGroup>
+          </Col>
+          <Col xs={12} md={12} className="mb-6">
+            <Form.Label className="input-label-text">İş Açıklaması</Form.Label>
+            <FormControl as="textarea" rows={6} name="description" className="form-control tobeto-input" />
+          </Col>
+        </Row>
+        <Button className="btn btn-primary py-2 mb-3 d-inline-block mobil-btn">Kaydet</Button>
+      </Form>
+      <Col xs={12}></Col>
+    </Col>
+  );
+};
+
+export default Experiences;
+
--- src/pages/Profile/ProfileEdit/ProfileEdit.tsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import SideMenu from '../../../components/ProfileInfo/SideMenu'
 import PersonalInfo from '../../../components/ProfileInfo/PersonalInfo'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import AuthBanner from '../../../components/AuthComponents/AuthBanner'
 import EducationLife from '../../../components/ProfileInfo/EducationLife'
 import SocialMedia from '../../../components/ProfileInfo/SocialMedia'
 import Abilities from '../../../components/ProfileInfo/Abilities'
+import Experiences from '../../../components/ProfileInfo/Experiences'
 
 const ProfileEdit = () => {
   return (
@@ -17,6 +17,7 @@ const ProfileEdit = () => {
         <Route path="/kisisel-bilgilerim" Component={PersonalInfo}></Route>
         <Route path='egitim-hayatim' Component={EducationLife}></Route>
         <Route path='sosyal-medya' Component={SocialMedia}></Route>
+        <Route path='deneyimlerim' Component={Experiences}></Route>
         <Route path="/a" Component={AuthBanner}></Route>
         <Route path="yetkinliklerim" Component={Abilities}></Route>
         </Routes>
