be53e61e5cd5b65e8efcc77ff2f3886ccf841e98
Hoixi <furkan.kayali.1905@gmail.com>
2024-01-16 21:12:02 +0300

    active

--- src/components/Navbar/NavbarComponent.tsx
+++ src/components/Navbar/NavbarComponent.tsx
@@ -1,8 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import { Navbar, Nav, Container, NavDropdown, Dropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
 
 function NavbarComponent() {
+  const location:any = useLocation(); // once ready it returns the 'window.location' object
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
+  
   return (
+    
     <Navbar expand="sm" className='position-relative navbar navbar-expand-xxl py-5 bg-white'>
       <Container fluid>
         <Navbar.Brand href="#home">
@@ -20,9 +29,9 @@ function NavbarComponent() {
             className="mx-auto my-lg-0 d-none d-xxl-flex navbar-nav"
             style={{ maxHeight: "100px" }}
             navbarScroll>
-            <Nav.Link as={Link} to={"/"} className='nav-active'>Ana Sayfa</Nav.Link>
-            <Nav.Link as={Link} to={"/profilim"}>Profilim</Nav.Link>
-            <Nav.Link as={Link} to={"/degerlendirmeler"}>Değerlendirmeler</Nav.Link>
+            <Nav.Link as={Link} to={"/"} className={(url === "/"? "nav-active" : "")}>Ana Sayfa</Nav.Link>
+            <Nav.Link as={Link} to={"/profilim"} className={(url === "/profilim"? "nav-active" : "")}>Profilim</Nav.Link>
+            <Nav.Link as={Link} to={"/degerlendirmeler"} className={(url === "/degerlendirmeler"? "nav-active" : "")}>Değerlendirmeler</Nav.Link>
             <Nav.Link>Katalog</Nav.Link>
             <Nav.Link>Takvim</Nav.Link>
             <Nav.Link>İstanbul Kodluyor</Nav.Link>
