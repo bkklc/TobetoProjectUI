c543a434b6f887bc4a17fd6f1351d99f0d60cab2
burakugras <burakugras67@gmail.com>
2024-01-16 23:13:56 +0300

    ProfileEdit languages added

--- src/pages/Profile/ProfileEdit/ProfileEdit.tsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.tsx
@@ -4,6 +4,7 @@ import SideMenu from '../../../components/ProfileInfo/SideMenu'
 import PersonalInfo from '../../../components/ProfileInfo/PersonalInfo'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AuthBanner from '../../../components/AuthComponents/AuthBanner'
+import MyLanguages from '../../../components/ProfileInfo/MyLanguages'
 
 const ProfileEdit = () => {
   return (
@@ -11,8 +12,9 @@ const ProfileEdit = () => {
       <Row>
         <SideMenu />
         <Routes>
-        <Route path="/kisisel-bilgilerim" Component={PersonalInfo}></Route>
-        <Route path="/a" Component={AuthBanner}></Route>
+          <Route path="/kisisel-bilgilerim" Component={PersonalInfo}></Route>
+          <Route path="/yabanci-dillerim" Component={MyLanguages}></Route>
+          <Route path="/a" Component={AuthBanner}></Route>
         </Routes>
       </Row>
     </Container>
