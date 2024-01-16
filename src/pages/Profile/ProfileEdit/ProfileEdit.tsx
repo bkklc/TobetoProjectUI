319c6f4a00c68599446c9d80530351b2998cfc52
burakugras <burakugras67@gmail.com>
2024-01-16 23:18:53 +0300

    ProfileEdit Routes

--- src/pages/Profile/ProfileEdit/ProfileEdit.tsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.tsx
@@ -2,7 +2,6 @@ import { Container, Row } from 'react-bootstrap'
 import SideMenu from '../../../components/ProfileInfo/SideMenu'
 import PersonalInfo from '../../../components/ProfileInfo/PersonalInfo'
 import { Route, Routes } from 'react-router-dom'
-import AuthBanner from '../../../components/AuthComponents/AuthBanner'
 import EducationLife from '../../../components/ProfileInfo/EducationLife'
 import SocialMedia from '../../../components/ProfileInfo/SocialMedia'
 import Abilities from '../../../components/ProfileInfo/Abilities'
@@ -15,8 +14,12 @@ const ProfileEdit = () => {
       <Row>
         <SideMenu />
         <Routes>
-        <Route path="/kisisel-bilgilerim" Component={PersonalInfo}></Route>
-        <Route path="/a" Component={AuthBanner}></Route>
+          <Route path="/kisisel-bilgilerim" Component={PersonalInfo}></Route>
+          <Route path='/egitim-hayatim' Component={EducationLife}></Route>
+          <Route path='/sosyal-medya' Component={SocialMedia}></Route>
+          <Route path='/deneyimlerim' Component={Experiences}></Route>
+          <Route path="/yetkinliklerim" Component={Abilities}></Route>
+          <Route path="/yabanci-dillerim" Component={MyLanguages}></Route>
         </Routes>
       </Row>
     </Container>
