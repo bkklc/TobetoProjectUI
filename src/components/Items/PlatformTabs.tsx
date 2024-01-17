5cf23ca9ac8c5959d6cb382852bc246082903e08
bkklc <abkklc@gmail.com>
2024-01-17 17:07:56 +0300

    PlatformTabs Edit

--- src/components/Items/PlatformTabs.tsx
+++ src/components/Items/PlatformTabs.tsx
@@ -1,9 +1,8 @@
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import { Card, Col, Container, Row } from "react-bootstrap";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 function PlatformTabs() {
-
   return (
     <Container>
       <Tabs
@@ -16,41 +15,92 @@ function PlatformTabs() {
           {/* Başvuru Field /Taşınabilir!! */}
           <Row>
             <Col md={6}>
-              <Card className='status-card status_accepted'>
+              <Card className="status-card status_accepted">
                 <span className="form_name">İstanbul Kodluyor</span>
                 <span className="form_status"></span>
                 <span className="pull_back"></span>
                 <span className="form_name">Bilgilendirme</span>
                 <div>
                   <div style={{ display: "flex", alignItems: "center" }}>
-                    <i className="fa fa-check fa-sm" aria-hidden="true" style={{ color: "green", marginRight: "5px" }} data-darkreader-inline-color=""></i>
-                    <span className="form_date"> İstanbul Kodluyor Başvuru Formu onaylandı.</span>
+                    <i
+                      className="fa fa-check fa-sm"
+                      aria-hidden="true"
+                      style={{ color: "green", marginRight: "5px" }}
+                      data-darkreader-inline-color=""
+                    ></i>
+                    <span className="form_date">
+                      {" "}
+                      İstanbul Kodluyor Başvuru Formu onaylandı.
+                    </span>
                   </div>
                   <div style={{ display: "flex", alignItems: "center" }}>
-                    <i className="fa fa-check fa-sm" aria-hidden="true" style={{ color: "green", marginRight: "5px" }} data-darkreader-inline-color=""></i>
-                    <span className="form_date"> İstanbul Kodluyor Belge Yükleme Formu onaylandı.</span>
+                    <i
+                      className="fa fa-check fa-sm"
+                      aria-hidden="true"
+                      style={{ color: "green", marginRight: "5px" }}
+                      data-darkreader-inline-color=""
+                    ></i>
+                    <span className="form_date">
+                      {" "}
+                      İstanbul Kodluyor Belge Yükleme Formu onaylandı.
+                    </span>
                   </div>
                 </div>
               </Card>
             </Col>
           </Row>
-
-
         </Tab>
         <Tab eventKey="courses" title="Eğitimlerim">
-          Tab content for Profile
+          <div className="col-md-3 col-12 mb-4">
+            <div className="corp-edu-card">
+              <div
+                className="card-img"
+                style={{
+                  backgroundImage:
+                    'url("https://tobeto.s3.cloud.ngn.com.tr/23_EAH_1_45f7232003.jpg")',
+                }}
+              />
+              <div className="card-content">
+                <div className="d-flex flex-column">
+                  <span>Dr. Ecmel Ayral'dan Hoşgeldin Mesajı</span>
+                  <span className="platform-course-date">
+                    21 Eylül 2023 15:20
+                  </span>
+                </div>
+                <a className="apply-btn">Eğitime Git</a>
+              </div>
+            </div>
+          </div>
         </Tab>
         <Tab eventKey="announcements" title="Duyuru ve Haberlerim">
-          Tab content for Loooonger Tab
+          <div className="row">
+            <div className="col-md-4 col-12 my-4">
+              <div className="notfy-card notify">
+                <div className="d-flex flex-column">
+                  <div className="d-flex justify-content-between mb-4">
+                    <span className="type">Duyuru</span>
+                    <span className="corp-names type">İstanbul Kodluyor</span>
+                  </div>
+                  <span className="header ">
+                    Ocak Ayı Tercih Formu Bilgilendirmesi
+                  </span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span className="date">12.01.2024</span>
+                  <span className="read-more">Devamını Oku</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </Tab>
         <Tab eventKey="surveys" title="Anketlerim">
-          Tab content for Contact
+          <div className=" noDataCard">
+            <p>Atanmış herhangi bir anketiniz bulunmamaktadır</p>
+          </div>
         </Tab>
       </Tabs>
     </Container>
-
   );
 }
 
 export default PlatformTabs;
-
