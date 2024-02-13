559d2c44e478b85ae0132710b44eb54c2ce2c47b
bkklc <abkklc@gmail.com>
2024-02-13 22:41:15 +0300

    Api connection added for survey

--- src/components/Platform/Items/SurveyComponent.tsx
+++ src/components/Platform/Items/SurveyComponent.tsx
@@ -1,8 +1,37 @@
+import ResponseData from "../../../hooks/ResponseData";
+import surveyService from "../../../services/surveyService";
+import { Row } from "react-bootstrap";
+
 const SurveyComponent = () => {
+  const surveyResponse = ResponseData(surveyService.getAll());
   return (
-    <div className=" noDataCard">
-      <p>Atanmış herhangi bir anketiniz bulunmamaktadır</p>
-    </div>
+    <Row>
+      {surveyResponse &&
+        surveyResponse.items.map((survey: any) => (
+          // <div className=" noDataCard">
+          //   <p>Atanmış herhangi bir anketiniz bulunmamaktadır</p>
+          // </div>
+
+          <div className="col-md-5 col-12 my-4">
+            <div className="notfy-card notify">
+              <div className="d-flex flex-column">
+                <div className="d-flex justify-content-between mb-4">
+                  <span className="type">Anket</span>
+                  <span className="corp-names type">İstanbul Kodluyor</span>
+                </div>
+                <span>{survey.name}</span>
+              </div>
+              <div className="d-flex-direction-column">
+                <span className="date">
+                  Başlangıç Tarihi: {survey.startedDate}
+                </span>
+                <span className="date">Bitiş Tarihi: {survey.endDate}</span>
+                <span className="read-more">Devamını Oku</span>
+              </div>
+            </div>
+          </div>
+        ))}
+    </Row>
   );
 };
 
