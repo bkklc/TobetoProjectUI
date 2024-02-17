3bda50ac8db3e9248e722a1a699998e3f37775d0
Beyza Altıntoprak <cicurit@gmail.com>
2024-02-18 01:06:01 +0300

    Footer and banner added to the gallery

--- src/pages/IstanbulCoding/Gallery/Gallery.tsx
+++ src/pages/IstanbulCoding/Gallery/Gallery.tsx
@@ -1,4 +1,37 @@
+import {
+  Envelope,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Twitter,
+} from "react-bootstrap-icons";
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <>
+      <div className="row bg-darkblue">
+        <div className="col-12 d-flex justify-content-center pt-10 pb-6">
+          <img
+            src="../../src/assets/png/istanbul-kodluyor-banner.png"
+            alt="İstanbul Kodluyor Logo"
+            decoding="async"
+            data-nimg="intrinsic"
+            style={{ width: "450px", height: "170px" }}
+          />
+        </div>
+      </div>
+
+      <div className="row bg-darkblue">
+        <div className="col-12 my-10 col-lg-auto d-flex justify-content-center gap-4">
+          <Facebook color="white" size={24} />
+          <Instagram color="white" size={24} />
+          <Twitter color="white" size={24} />
+          <Linkedin color="white" size={24} />
+          <Envelope color="white" size={24} />
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default Gallery;
